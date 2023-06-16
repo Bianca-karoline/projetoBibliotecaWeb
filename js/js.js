228f6ff638095f3421178003c64d5476991af7a1
Bianca Karoline <biancakarolineferreira14@gmail.com>
2023-06-16 14:33:38 -0300

    feat

---
+++ js/js.js
@@ -0,0 +1,18 @@
+function exibirform(id){
+    let display = document.getElementById(id).style.display;
+        if(display == "none"){
+            document.getElementById(id).style.display = 'block';
+        }
+        else{
+            document.getElementById(id).style.display = 'none';
+        }
+}
+function editar(id){
+    let display = document.getElementById(id).style.display;
+        if(display == "none"){
+            document.getElementById(id).style.display = 'block';
+        }
+        else{
+            document.getElementById(id).style.display = 'none';
+        }
+}
