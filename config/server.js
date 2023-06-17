9868136a74052bf501b778a1e5e3d05dd3604c3f
Leonardo Emanuel Feitosa <116128056+leoemfs@users.noreply.github.com>
2023-06-17 02:14:41 -0300

    server configurado

--- config/server.js
+++ config/server.js
@@ -1,1 +1,12 @@
 //@method criar server port 6000
+const mysql = require('mysql2');
+
+//config da conn db
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: '1234',
+  database: 'biblioteca_db'
+});
+
+module.exports = connection;
