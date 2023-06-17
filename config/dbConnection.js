40b195b8e3de221bdb97efb756877e0b7014c5af
Leonardo Emanuel Feitosa <116128056+leoemfs@users.noreply.github.com>
2023-06-17 02:13:53 -0300

    conexao banco

--- config/dbConnection.js
+++ config/dbConnection.js
@@ -1,1 +1,12 @@
 //@method criar conexão
+const mysql = require('mysql2');
+
+//config da conn db
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: '1234',
+  database: 'biblioteca_db'
+});
+
+module.exports = connection;
